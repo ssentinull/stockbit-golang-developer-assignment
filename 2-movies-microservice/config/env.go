3ad41c0c6338b65869eaa2193cf1e47acde7e4f2
IbnuAhsani <ibnu.muhari@gmail.com>
2021-03-21 11:45:21 +0700

    feat: mysql db credentials .env values & getter function

--- 2-movies-microservice/config/env.go
+++ 2-movies-microservice/config/env.go
@@ -25,6 +25,18 @@ func Env() string {
 	return "development"
 }
 
+func MySQLDBDSN() string {
+	dbUser := os.Getenv("MYSQL_DB_USER")
+	dbPassword := os.Getenv("MYSQL_DB_PASSWORD")
+	dbProtocol := os.Getenv("MYSQL_DB_PROTOCOL")
+	dbHost := os.Getenv("MYSQL_DB_HOST")
+	dbPort := os.Getenv("MYSQL_DB_PORT")
+	dbName := os.Getenv("MYSQL_DB_NAME")
+
+	return fmt.Sprintf("%v:%v@%v(%v:%v)/%v?parseTime=true",
+		dbUser, dbPassword, dbProtocol, dbHost, dbPort, dbName)
+}
+
 func OMDBKey() string {
 	val, ok := os.LookupEnv("OMDB_API_KEY")
 	if !ok {
